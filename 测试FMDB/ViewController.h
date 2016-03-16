f2d7c336d8e3da505ca16062891016edc677d398
beike柯 <406495729@qq.com>
2016-03-16 22:23:48 +0800

    第一版

---
+++ 测试FMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试FMDB
+//
+//  Created by beike柯 on 16/3/16.
+//  Copyright © 2016年 beike柯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 测试FMDB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  测试FMDB
+//
+//  Created by beike柯 on 16/3/16.
+//  Copyright © 2016年 beike柯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
